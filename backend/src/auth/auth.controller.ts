731fe3d87d59d1b6c594c22f71d2c303459040df
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-08-11 22:24:17 -0500

    Se incluye auth.controller.ts backend

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterUserDto } from '../dtos/register-user.dto';
+import { LoginUserDto } from '../dtos/login-user.dto';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService) { }
+
+    @Post('register')
+    async register(@Body() registerDto: RegisterUserDto) {
+        return this.authService.register(registerDto);
+    }
+
+    @Post('login')
+    @HttpCode(HttpStatus.OK)
+    async login(@Body() loginDto: LoginUserDto) {
+        return this.authService.login(loginDto);
+    }
+}
