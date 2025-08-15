82ee6df5275c1ba3d56a9e7ce7de2c03d2ee7ae8
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-08-14 22:38:01 -0500

    Se incluye user.contoller.ts

---
+++ backend/src/user/user.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import { UserService } from './user.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+
+@Controller('users')
+export class UserController {
+    constructor(private readonly userService: UserService) { }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('me')
+    getProfile(@Req() req) {
+        return this.userService.findById(req.user.userId);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    findAll(@Req() req) {
+        // Devuelve todos los usuarios excepto el actual
+        return this.userService.findAllExcept(req.user.userId);
+    }
+}
