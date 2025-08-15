565caf135ca8b117407a6228fe8570527fb7e680
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-08-14 22:51:15 -0500

    Se incluye message.entity.ts

---
+++ backend/src/message/entity/message.entity.ts
@@ -0,0 +1,23 @@
+import { User } from 'src/user/user.entity';
+import { CreateDateColumn, Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+
+@Entity()
+export class Message {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'text', nullable: true })
+    text: string;
+
+    @Column({ nullable: true })
+    audioUrl: string;
+
+    @ManyToOne(() => User, (user) => user.sentMessages)
+    sender: User;
+
+    @ManyToOne(() => User, (user) => user.receivedMessages)
+    receiver: User;
+
+    @CreateDateColumn()
+    createdAt: Date;
+}
