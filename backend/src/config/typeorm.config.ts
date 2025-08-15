8195d6e694579340aa9807873e37f83defbb8d92
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-08-14 19:30:30 -0500

    Se incluye typeorm.config.ts

---
+++ backend/src/config/typeorm.config.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { Message } from 'src/message/message.entity';
+import { User } from 'src/user/user.entity';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+    constructor(private configService: ConfigService) {}
+
+    createTypeOrmOptions(): TypeOrmModuleOptions {
+        return {
+            type: 'mysql',
+            host: this.configService.get<string>('DB_HOST'),
+            port: parseInt(this.configService.get<string>('DB_PORT')),
+            username: this.configService.get<string>('DB_USER'),
+            password: this.configService.get<string>('DB_PASSWORD'),
+            database: this.configService.get<string>('DB_NAME'),
+            entities: [User, Message],
+            synchronize: true, // Nota: Usar solo en desarrollo
+        };
+    }
+}
