b4d9aad34691e6860cb544decebc21a444cbbef7
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-08-14 22:41:41 -0500

    Se incluye user.entity.ts

---
+++ backend/src/user/entity/user.entity.ts
@@ -0,0 +1,23 @@
+import { Message } from 'src/message/message.entity';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    password: string;
+
+    @OneToMany(() => Message, (message) => message.sender)
+    sentMessages: Message[];
+
+    @OneToMany(() => Message, (message) => message.receiver)
+    receivedMessages: Message[];
+}
