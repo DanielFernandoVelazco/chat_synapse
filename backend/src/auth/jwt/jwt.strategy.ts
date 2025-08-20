b49528b2537f383a76f3e86f2bdb00fb85a77532
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-08-20 18:53:49 -0500

    Se modifica jwt.strategy.ts

--- backend/src/auth/jwt/jwt.strategy.ts
+++ backend/src/auth/jwt/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: configService.get<string>('JWT_SECRET'),
+            secretOrKey: configService.get<string>('JWT_SECRET') || 'default-secret',
         });
     }
     async validate(payload: any) {
